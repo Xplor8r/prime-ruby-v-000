cd048130574fb127ccde7cb8f74b43e316ab148f
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-27 03:48:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(num)
-  FALSE if num <= 1
+  return FALSE if num <= 1
   Math.sqrt(num).to_i.downto(2).each do |i|
-    FALSE if num % i == 0
+    return FALSE if num % i == 0
   end
   TRUE
 end
