aaf61023d0b2d8fa348c46c768216b4164d21004
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-27 03:06:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(int)
   if int <= 0
-    (2..Math.sqrt(int)).none? { |i| (int % i).zero? }
-    TRUE
+  (2..Math.sqrt(int)).none? { |i| (int % i).zero? }
+  TRUE
   else
-    FALSE
+  FALSE
   end
 end
